edd404232c9636d4b715a5b8ac2df0f96e0f9f44
adrianugb <adriangbonilla49@gmail.com>
2025-07-29 23:41:40 -0600

    primera parte

--- API/models/Estudiantes.js
+++ API/models/Estudiantes.js
@@ -1,16 +1,44 @@
 const mongoose = require('mongoose');
 
-const EstudiantesSchema = new mongoose.Schema(
+const CalificacionesSchema = new mongoose.Schema(
     {
         //id: { type: Number, required: true, unique: true},
         id: { type: Number},
-        nombre: { type: String},
-        carrera: { type: String},
-        cuatrimestre: { type: String},
-	    correo: { type: String},
-        estado: { type: String}
+        nombre: {
+        type: String,
+        required: true,
+        match: [
+        /^([A-Za-zÁÉÍÓÚáéíóúÑñ]+ ){3}[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/,
+        'El nombre debe tener dos nombres y dos apellidos, sin números ni símbolos.'
+        ]
+    },
+        cuatrimestre: {
+            type: Number,
+            required: true,
+            index: true
+    },
+        notaMatematicas: {
+            type: Number,
+            required: true
+    },
+        notaCiencias: {
+            type: Number,
+            required: true
+    },
+        notaLiteratura: {
+            type: Number,
+            required: true
+    },
+        notaCivica: {
+            type: Number,
+            required: true
+    },
+        condicion: {
+            type: String,
+            enum: ['Aprobado', 'Ampliación', 'Reprobado'],
+            required: true
     }
-);
+    });
 
 
-module.exports = mongoose.model('Estudiantes', EstudiantesSchema);
+module.exports = mongoose.model('Calificaciones', CalificacionesSchema, 'calificaciones');
--- API/server.js
+++ API/server.js
@@ -2,26 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParse = require('body-parser');
 
-const RouterCurso = require('./routers/CursoRourtes');
 const RouterEstudiantes = require('./routers/EstudiantesRourtes');
-const RouterSede = require('./routers/SedeRourtes');
-const RouterProfesores = require('./routers/ProfesoresRourtes');
 
 const cors = require('cors');
 const app = express();
 const PORT = 3000;
-mongoose.connect('mongodb://127.0.0.1:27017/academias',{
+mongoose.connect('mongodb://127.0.0.1:27017/simulacion2',{
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 app.use(cors());
 app.use(bodyParse.json());
-
-app.use('/api/cursos', RouterCurso)
-app.use('/api/estudiantes', RouterEstudiantes)
-app.use('/api/sedes', RouterSede)
-app.use('/api/profesores', RouterProfesores)
-
+app.use('/api/calificaciones', RouterEstudiantes)
 app.listen(PORT, ()=> {
         //version old
         //console.log('Servidor corriendo http://localhost:'+PORT);
